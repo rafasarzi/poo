463716cfe104ea55ff609a13c598186021788ce6
rafasarzi <rajustino@gmail.com>
2022-07-25 16:08:52 -0300

    code refactoring and create subclassing

--- Personagem.ts
+++ Personagem.ts
@@ -1,63 +1,14 @@
-export default class Personagem {
-    constructor(
-        private _nome: string,
-        private energia: number,
-        private vida: number,
-        private ataque: number,
-        private defesa: number
-    ) {}
+import { Util } from "./Util";
+export class Personagem {
+    protected _nome: string;
+    protected _armadura: number;
+    protected _vidaMaxima: number;
+    protected _vidaAtual: number;
 
-    //  TODA VEZ QUE PENSAR EM LER DADOS NA CLASSE - USE PARAMETRO
-    //  TODA VEZ QUE PENSAR EM IMPRIMIR NA CLASSE, USE RETORNO
-
-    public get nome(): string {
-        return this._nome;
-    }
-
-    public set nome(nome: string) {
+    constructor(nome: string) {
         this._nome = nome;
-    }
-
-    public status(): string {
-        return (
-            "Guerreiro: \n" +
-            "\nNome: " +
-            this.nome +
-            ("\nEnergia: " + this.energia.toFixed(1)) +
-            ("\nAtaque: " + this.ataque.toFixed(1)) +
-            ("\nDefesa: " + this.defesa.toFixed(1))
-        );
-    }
-    public treinarAtacar(): void {
-        this.ataque += this.randomizar(7);
-        this.energia -= this.randomizar(5);
-        if (this.defesa > 100) {
-            this.defesa = 0;
-        }
-    }
-    public treinarDefesa(): void {
-        this.defesa += this.randomizar(5);
-        this.energia -= this.randomizar(10);
-        if (this.defesa > 100) {
-            this.defesa = 0;
-        }
-    }
-    public descansar(hour: number): void {
-        this.energia += hour * this.randomizar(10);
-        if (this.energia > 100) {
-            this.energia = 100;
-        }
-    }
-    public batalhar(): number {
-        let desgaste: number = this.randomizar(10);
-        this.energia -= desgaste;
-        return desgaste;
-    }
-    public isDead(): boolean {
-        return this.energia < 0;
-    }
-
-    private randomizar(fator: number): number {
-        return Math.random() * fator;
+        this._armadura = Util.randomizar(100, 1_000);
+        this._vidaMaxima = Util.randomizar(100, 1_000);
+        this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
     }
 }
--- main.ts
+++ main.ts
@@ -1,49 +1,12 @@
-import Personagem  from "./Personagem";
-import prompt from "prompt-sync";
+import { Priest } from "./Priest";
+import { Warrior } from "./Warrior";
+import { Mage } from "./Mage";
+import { Personagem } from "./Personagem";
 
-let person: Personagem = new Personagem("Sansa Stark", 100, 40, 20, 20);
+let mage: Personagem = new Mage("Angelo mage");
+let warrior: Personagem = new Warrior("Angelo warrior");
+let priest: Personagem = new Priest("Angelo priest");
 
-let teclado = prompt();
-let option: number = 0;
-
-while (option != 9 || person.isDead()) {
-    console.log(`+========= Personagem ${person.nome}=============`);
-    console.log("|1. Treinar ataque                 |");
-    console.log("|2. Treinar defesa                 |");
-    console.log("|3. Descansar                      |");
-    console.log("|4. Entrar em batalha              |");
-    console.log("|8. Imprimir atributos             |");
-    console.log("|9. Sair                           |");
-    console.log("+==================================+");
-
-    option = +teclado("Escolha uma ação: ");
-
-    switch (option) {
-        case 1:
-            person.treinarAtacar();
-
-            console.log(person.status());
-            break;
-        case 2:
-            person.treinarDefesa();
-            console.log(person.status());
-            break;
-        case 3:
-            let horas: number = +teclado("Digite o número de horas: ");
-            person.descansar(horas);
-            console.log(person.status());
-            break;
-        case 4:
-            let rest: number = person.batalhar();
-            console.log(`Esta batalha custo ${rest} de energia`);
-
-            console.log(person.status());
-            break;
-        case 8:
-            console.log(person.status());
-            break;
-        default:
-            break;
-    }
-}
-console.log("Ops! Morreu!");
+console.log("mage :>> ", mage);
+console.log("warrior :>> ", warrior);
+console.log("priest :>> ", priest);
